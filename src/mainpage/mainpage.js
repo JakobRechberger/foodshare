9448938235561674589b714f1d010745368341a0
Rechberger, Jakob <jakob.rechberger@student.fhws.de>
2023-12-29 11:45:23 +0100

    mainpage header and favicon

--- src/mainpage/mainpage.js
+++ src/mainpage/mainpage.js
@@ -1,11 +1,26 @@
+import React from 'react';
+import './mainpage.css'
+import logo from '../assets/media/FoodShare_1.png'
+import search from '../assets/media/search.svg'
 
 
 function MainPage() {
     return (
-       <div>
-           Hello World
-       </div>
+        <div className={"main-page"}>
+            <div className={"title-box"}>
+                <img src={logo} alt={"icon"} height={100} width={100}/>
+                <span className={"title-text"}>FoodShare</span>
+                <div className="search">
+                    <input type="text" className="searchTerm" placeholder="Search"></input>
+                        <button type="submit" className="searchButton">
+                            <img src={search} alt={"search-icon"}/>
+                        </button>
+                </div>
+            </div>
+
+        </div>
     );
 }
 
 export default MainPage;
+
